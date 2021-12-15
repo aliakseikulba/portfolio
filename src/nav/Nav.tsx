836f9a8956ad1564f0209b1d76d4dabde5b7c83c
aliakseikulba <kulba.av@gmail.com>
2021-12-15 16:58:09 +0300

    Nav

---
+++ src/nav/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Nav.module.css';
+
+const Nav = () => {
+  return (
+    <div className={s.nav}>
+      <a href="">main</a>
+      <a href="">skills</a>
+      <a href="">projects</a>
+      <a href="">contacts</a>
+    </div>
+  );
+};
+
+export {Nav};
