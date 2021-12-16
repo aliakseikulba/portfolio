9bb2b452b4aa0d8fff80e96ae913a06c8568ce0b
aliakseikulba <kulba.av@gmail.com>
2021-12-16 14:32:21 +0300

    Project

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Projects.module.css';
 import stylesContainer from './../common/styles/Container.module.css'
+import {Project} from './project/Project';
 
 const Projects = () => {
   return (
@@ -8,7 +9,10 @@ const Projects = () => {
       <div className={`${stylesContainer.container} ${s.projectsContainer}`}>
         <h2 className={s.title}>My Projects</h2>
         <div className={s.projects}>
-
+          <Project/>
+          <Project/>
+          <Project/>
+          <Project/>
         </div>
       </div>
     </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import s from './Project.module.css'
 
 const Project = () => {
   return (
-    <div>
-
+    <div className={s.project}>
+      <div className={s.view}>
+        <a href="">
+          <button className={s.lookBtn}>View</button>
+        </a>
+      </div>
+      <h3 className={s.projectName}>Project Name</h3>
+      <p className={s.description}>Short project description</p>
     </div>
   );
 };
